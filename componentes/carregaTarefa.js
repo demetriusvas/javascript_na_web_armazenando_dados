6a1113fe30b620a939de76acd5ea32ad1c2116c9
Demetrius Vignati <demetriusvas@gmail.com>
2021-12-09 17:22:18 -0300

    conclusão do projeto

--- componentes/carregaTarefa.js
+++ componentes/carregaTarefa.js
@@ -1,12 +1,15 @@
-import { Tarefa } from "./criaTarefa.js"
+import { ordenaDatas, removeDatasRepetidas } from "../service/data.js"
+import { criaData } from "./criaData.js"
 
 export const carregaTarefa = () => { 
     const lista = document.querySelector('[data-list]')
 
-    const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas'))||[]
+    const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas')) ||[]
 
     lista.innerHTML = " "
-    tarefasCadastradas.forEach((tarefa) => {
-        lista.appendChild(Tarefa(tarefa))
+    const datasUnicas = removeDatasRepetidas(tarefasCadastradas)
+    ordenaDatas(datasUnicas)
+    datasUnicas.forEach((dia) => {
+        lista.appendChild(criaData(dia))
     })
 }
--- componentes/concluiTarefa.js
+++ componentes/concluiTarefa.js
@@ -1,22 +1,20 @@
-const BotaoConclui = () => { 
+const concluirTarefa = (atualiza, id) => {
+    const tarefaCadastrada = JSON.parse(localStorage.getItem('tarefas'))
+
+    tarefaCadastrada[id].concluida = !tarefaCadastrada[id].concluida
+    localStorage.setItem('tarefas', JSON.stringify(tarefaCadastrada))
+
+    atualiza()
+}
+
+const BotaoConclui = (atualiza, id) => { 
     const botaoConclui = document.createElement('button')  
     
     botaoConclui.classList.add('check-button')
     botaoConclui.innerText = 'concluir'
 
-    botaoConclui.addEventListener('click', concluirTarefa)
+    botaoConclui.addEventListener('click', ()=>concluirTarefa(atualiza, id))
 
     return botaoConclui
-
 }
-
-const concluirTarefa = (evento) => {
-    const botaoConclui = evento.target
-
-    const tarefaCompleta = botaoConclui.parentElement
-
-    tarefaCompleta.classList.toggle('done')
-}
-
 export default BotaoConclui
-
